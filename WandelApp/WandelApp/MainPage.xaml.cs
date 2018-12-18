ca65be8c9e97f37c55877fe82dd6f4a3715403e5
Justin Muris <1736302muris@zuyd.nl>
2018-12-18 12:16:01 +0100

    Geklungel met MainPage.xaml views. Sliders en Switch toegevoegd om de Preferences te kunnen wijzigen.

--- WandelApp/WandelApp/MainPage.xaml.cs
+++ WandelApp/WandelApp/MainPage.xaml.cs
@@ -9,9 +9,47 @@ namespace WandelApp
 {
     public partial class MainPage : ContentPage
     {
+        private Models.Preferences prefs;
         public MainPage()
         {
             InitializeComponent();
+            prefs = new Models.Preferences();
+        }
+
+        private void Slider_Hill_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (Slider_Hill.Value >= 0 && Slider_Hill.Value < 1)
+            {
+                prefs.hills = Models.Hills.None;
+            }
+            else if (Slider_Hill.Value >= 1 && Slider_Hill.Value < 2)
+            {
+                prefs.hills = Models.Hills.Sloped;
+            }
+            else if (Slider_Hill.Value >= 2)
+            {
+                prefs.hills = Models.Hills.Steep;
+            }
+
+            Label_Hill.Text = prefs.hills.ToString();
+        }
+
+        private void Slider_Forest_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (Slider_Forest.Value >= 0 && Slider_Forest.Value < 1)
+            {
+                prefs.forestDensity = Models.ForestDensity.None;
+            }
+            else if (Slider_Forest.Value >= 1 && Slider_Forest.Value < 2)
+            {
+                prefs.forestDensity = Models.ForestDensity.Thin;
+            }
+            else if (Slider_Forest.Value >= 2)
+            {
+                prefs.forestDensity = Models.ForestDensity.Thick;
+            }
+
+            Label_Forest.Text = prefs.forestDensity.ToString();
         }
     }
 }
---
+++ WandelApp/WandelApp/Models/Preferences.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WandelApp.Models
+{
+    public enum Hills { None, Sloped, Steep };
+    public enum ForestDensity { None, Thin, Thick };
+
+    class Preferences
+    {
+        public Hills hills = Hills.None;
+        public ForestDensity forestDensity = ForestDensity.Thick;
+        public bool asphaltedRoad = true;
+
+    }
+}
