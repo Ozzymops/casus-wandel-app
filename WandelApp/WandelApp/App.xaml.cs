9f7580256d5aa191080c9fc6908f381e8f969eca
justinmuris <justinmuris@gmail.com>
2018-12-21 16:00:24 +0100

    LogInPage and RegisterPage added

--- WandelApp/WandelApp/App.xaml.cs
+++ WandelApp/WandelApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace WandelApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Views.RegisterPage(); // reset naar MainPage
         }
 
         protected override void OnStart()
