970567002717b76e6b4140e1851ed75e02cfceb9
Roy van der Kaap <roy_vd_kaap@hotmail.com>
2018-12-23 13:56:31 +0100

    hoop GUI

--- WandelApp/WandelApp/Views/NewMainPageDetail.xaml.cs
+++ WandelApp/WandelApp/Views/NewMainPageDetail.xaml.cs
@@ -12,9 +12,62 @@ namespace WandelApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewMainPageDetail : ContentPage
     {
+        private Models.Preferences prefs;
         public NewMainPageDetail()
         {
             InitializeComponent();
+            prefs = new Models.Preferences();
         }
+
+        private void Slider_Hill_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (Slider_Hill.Value >= 0 && Slider_Hill.Value < 1)
+            {
+                prefs.hills = Models.Hills.None;
+            }
+            else if (Slider_Hill.Value >= 1 && Slider_Hill.Value < 2)
+            {
+                prefs.hills = Models.Hills.Sloped;
+            }
+            else if (Slider_Hill.Value >= 2)
+            {
+                prefs.hills = Models.Hills.Steep;
+            }
+
+            Label_Hill.Text = prefs.hills.ToString();
+        }
+
+        private void Slider_Forest_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (Slider_Forest.Value >= 0 && Slider_Forest.Value < 1)
+            {
+                prefs.forestDensity = Models.ForestDensity.None;
+            }
+            else if (Slider_Forest.Value >= 1 && Slider_Forest.Value < 2)
+            {
+                prefs.forestDensity = Models.ForestDensity.Thin;
+            }
+            else if (Slider_Forest.Value >= 2)
+            {
+                prefs.forestDensity = Models.ForestDensity.Thick;
+            }
+
+            Label_Forest.Text = prefs.forestDensity.ToString();
+        }
+
+        private void Slider_Flatness_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+
+        private void Slider_Signs_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+
+        //private void Switch_Toggled(object sender, ToggledEventArgs e)
+        //{
+
+        //}
     }
 }
